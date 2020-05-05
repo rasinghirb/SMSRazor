a94c92ef238e534ad41edc73392dde7a19fc6c39
rasinghirb <rasinghirb@gmail.com>
2020-05-05 11:35:18 +0530

    Add project files.

---
+++ Models/ApplicationContextDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMSRazor.Models
+{
+    public class ApplicationContextDb:DbContext
+    {
+        public ApplicationContextDb(DbContextOptions<ApplicationContextDb>options):base(options)
+        {
+
+        }
+        public DbSet<Books> Book { get; set; }
+    }
+}
